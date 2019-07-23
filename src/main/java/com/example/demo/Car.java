9b903df99ff6f4250ae4946294e33b12159654cd
nboober <nboober@terpmail.umd.edu>
2019-07-23 09:50:26 -0400

    Added all my variables to my car and category classes

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Car {
@@ -12,4 +12,71 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @NotNull
+    @Size(min=3)
+    private String manufacturer;
+
+    @NotNull
+    @Size(min=3)
+    private String model;
+
+    @NotNull
+    @Size(min=4)
+    private int year;
+
+    @NotNull
+    @Size(min=3)
+    private int msrp;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category.id")
+    private Category category;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(int msrp) {
+        this.msrp = msrp;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/example/demo/Category.java
+++ src/main/java/com/example/demo/Category.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
 public class Category {
@@ -12,4 +13,36 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
 
+    @NotNull
+    @Size(min = 3)
+    private String name;
+
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    public Set<Car> cars;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars = cars;
+    }
 }
